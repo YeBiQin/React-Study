2e352f07409372cb8dfb031afbd3682e024cc848
yebiqin <biqin.ye@notta.ai>
2021-01-18 16:06:36 +0800

    引入chakara框架

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import App from './pages/app'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { extendTheme, ChakraProvider } from "@chakra-ui/react"
+
+// 可选的自定义主题
+const themeParam = extendTheme({
+  colors: {
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac"
+    }
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider theme={themeParam}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
   document.getElementById('root')
 );
